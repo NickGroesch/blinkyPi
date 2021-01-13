4d7dc7da79d74ccd9d71afcebe14900d130cd9de
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2021-01-12 19:31:16 -0600

    better explanation of reduce as superset of array and filter

--- swissArmyReduce.js
+++ swissArmyReduce.js
@@ -1,12 +1,14 @@
 //DOING IT WRONG: using Array.reduce as Array.filter
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b"]
-const evens = array.reduce((accum, elem) => {
+const DUBevens = array.reduce((accum, elem) => {
     console.log(accum, elem)
-    if (elem % 2 == 0) {
-        accum.push(elem)
+    if (elem % 2 == 0) {//FILTER callback
+        accum.push(elem * 2)//MAP might complicate the expression in here
         return accum
     } else {
         return accum
     }
 }, [])
-console.log(evens)
+console.log(DUBevens)
+
+//MAP AND FILTER RETURNS CAN BE SIMULATED BY THE ACCUMULATOR PARAMETER ADDED IN THE REDUCE FILTER
