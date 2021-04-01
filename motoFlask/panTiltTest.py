52414bd8d6819a22c77743d802580672316d562f
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2021-04-01 13:22:07 -0500

    one single value at a time please

--- motoFlask/panTiltTest.py
+++ motoFlask/panTiltTest.py
@@ -5,31 +5,9 @@ from PCA9685 import PCA9685
 
 print("Testing pantilt PCA9685 routine")
 pwm = PCA9685(0x40, debug=True)
+pwm.start_PCA9685()
 pwm.setPWMFreq(50)
-# pwm.setServoPulse(1,500)
-pwm.setRotationAngle(1, 0)
 
-try:
-    for i in range(10,170,1): 
-        print(i)
-        pwm.setRotationAngle(1, i)   
-        if(i<80):
-            pwm.setRotationAngle(0, i)   
-        time.sleep(0.1)
-
-    for i in range(170,10,-1): 
-        print(i)
-        pwm.setRotationAngle(1, i)   
-        if(i<80):
-            pwm.setRotationAngle(0, i)            
-        time.sleep(0.1)
-
-
-    pwm.exit_PCA9685()
-    print("\nProgram end")
-    exit()
-
-except:
-    pwm.exit_PCA9685()
-    print("\nProgram dies")
-    exit()
+first=input('0 val')
+second=input('1000val')
+pwm.setServoPulse(first, second)
