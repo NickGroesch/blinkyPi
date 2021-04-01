c545d77816eac2c087a7c96e1d75bef3131b91b7
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2021-04-01 12:51:12 -0500

    I think this is better python

--- motoFlask/panTiltTest.py
+++ motoFlask/panTiltTest.py
@@ -3,22 +3,22 @@ import time
 import RPi.GPIO as GPIO
 from PCA9685 import PCA9685
 
+print("Testing pantilt PCA9685 routine")
+pwm = PCA9685()
+pwm.setPWMFreq(50)
+# pwm.setServoPulse(1,500)
+pwm.setRotationAngle(1, 0)
+
 try:
-    print("This is an PCA9685 routine")
-    pwm = PCA9685()
-    pwm.setPWMFreq(50)
-    # pwm.setServoPulse(1,500)
-    pwm.setRotationAngle(1, 0)
-
-   
-
     for i in range(10,170,1): 
-            pwm.setRotationAngle(1, i)   
-            if(i<80):
-                pwm.setRotationAngle(0, i)   
-            time.sleep(0.1)
+        print(i)
+        pwm.setRotationAngle(1, i)   
+        if(i<80):
+            pwm.setRotationAngle(0, i)   
+        time.sleep(0.1)
 
     for i in range(170,10,-1): 
+        print(i)
         pwm.setRotationAngle(1, i)   
         if(i<80):
             pwm.setRotationAngle(0, i)            
@@ -31,5 +31,5 @@ try:
 
 except:
     pwm.exit_PCA9685()
-    print("\nProgram end")
+    print("\nProgram dies")
     exit()
